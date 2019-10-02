1946a68e88e5c057b0494b7152e0384fd2db803f
Patrick Pokatilo <mail@shyxormz.net>
2019-10-02 14:26:12 +0200

    Fix typo

--- pipeline_stage.go
+++ pipeline_stage.go
@@ -15,7 +15,7 @@ const (
 	PipelineStageEarlyFragmentTestsBit
 	PipelineStageLateFragmentTestsBit
 	PipelineStageColorAttachmentOutputBit
-	PipelienStageComputeShaderBit
+	PipelineStageComputeShaderBit
 	PipelineStageTransferBit
 	PipelineStageBottomOfPipeBit
 	PipelineStageHostBit
