60e28f05b41ccde799324056cd43e7dd5a4f672a
Patrick Pokatilo <mail@shyxormz.net>
2022-09-05 07:39:18 +0200

    Add CmdCopyBuffer and CmdCopyBuffer2

--- buffer.go
+++ buffer.go
@@ -1,5 +1,12 @@
 package vulkan
 
+// #include <stdlib.h>
+// #include <vulkan/vulkan.h>
+import "C"
+import (
+	"unsafe"
+)
+
 type Buffer uintptr
 
 type BufferCreateFlagBits uint32
@@ -35,4 +42,81 @@ const (
 	BufferUsageIndexBufferBit
 	BufferUsageVertexBufferBit
 	BufferUsageIndirectBufferBit
+)
+
+type BufferCopy struct {
+	SrcOffset DeviceSize
+	DstOffset DeviceSize
+	Size      DeviceSize
+}
+
+func CmdCopyBuffer(commandBuffer CommandBuffer, srcBuffer, dstBuffer Buffer, regions []BufferCopy) {
+	var regionPtr unsafe.Pointer
+	if len(regions) > 0 {
+		regionPtr = unsafe.Pointer(&regions[0])
+	}
+	C.vkCmdCopyBuffer(
+		*(*C.VkCommandBuffer)(unsafe.Pointer(&commandBuffer)),
+		*(*C.VkBuffer)(unsafe.Pointer(&srcBuffer)),
+		*(*C.VkBuffer)(unsafe.Pointer(&dstBuffer)),
+		(C.uint32_t)(len(regions)),
+		(*C.VkBufferCopy)(regionPtr),
+	)
+}
+
+type CopyBufferInfo2 struct {
+	Type      StructureType
+	Next      unsafe.Pointer
+	SrcBuffer Buffer
+	DstBuffer Buffer
+	Regions   []BufferCopy2
+}
+
+type copyBufferInfo2 struct {
+	Type        StructureType
+	Next        unsafe.Pointer
+	SrcBuffer   Buffer
+	DstBuffer   Buffer
+	RegionCount uint32
+	RegionPtr   *BufferCopy2
+}
+
+func (info *CopyBufferInfo2) C(_info *copyBufferInfo2) freeFunc {
+	var regionPtr unsafe.Pointer
+	if len(info.Regions) > 0 {
+		regionPtr = copySliceToC(nil, info.Regions)
+	}
+	*_info = copyBufferInfo2{
+		Type:        info.Type,
+		Next:        info.Next,
+		SrcBuffer:   info.SrcBuffer,
+		DstBuffer:   info.DstBuffer,
+		RegionCount: uint32(len(info.Regions)),
+		RegionPtr:   (*BufferCopy2)(regionPtr),
+	}
+	return func() {
+		C.free(regionPtr)
+	}
+}
+
+type BufferCopy2 struct {
+	Type      StructureType
+	Next      unsafe.Pointer
+	SrcOffset DeviceSize
+	DstOffset DeviceSize
+	Size      DeviceSize
+}
+
+func CmdCopyBuffer2(commandBuffer CommandBuffer, info CopyBufferInfo2) {
+	var _info copyBufferInfo2
+	info.C(&_info)
+	C.vkCmdCopyBuffer2(
+		*(*C.VkCommandBuffer)(unsafe.Pointer(&commandBuffer)),
+		(*C.VkCopyBufferInfo2)(unsafe.Pointer(&_info)),
+	)
+}
+
+const (
+	StructureTypeCopyBufferInfo2 StructureType = 1000337000
+	StructureTypeBufferCopy2     StructureType = 1000337006
 )
