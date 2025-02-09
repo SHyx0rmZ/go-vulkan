87aef2f4a231c57bc479a4c409a15e2de7a9f43d
Patrick Pokatilo <mail@shyxormz.net>
2025-02-09 14:38:19 +0100

    MapMemory now returns unsafe.Pointer instead of uintptr

--- command_buffers.go
+++ command_buffers.go
@@ -363,12 +363,12 @@ func FreeMemory(device Device, memory DeviceMemory, allocator *AllocationCallbac
 //   - ErrorOutOfHostMemory
 //   - ErrorOutOfDeviceMemory
 //   - ErrorMemoryMapFailed
-func MapMemory(device Device, memory DeviceMemory, offset, size DeviceSize, flags MemoryMapFlags) (uintptr, error) {
+func MapMemory(device Device, memory DeviceMemory, offset, size DeviceSize, flags MemoryMapFlags) (unsafe.Pointer, error) {
 	//const minMemoryMapAlignment = 4096
 	//var data [minMemoryMapAlignment / unsafe.Sizeof(uintptr(0))]uintptr
 	//addr := uintptr(unsafe.Pointer(&data[0]))
 	//idx := (((addr + minMemoryMapAlignment - 1) & ^uintptr(minMemoryMapAlignment-1)) - addr) / unsafe.Sizeof(uintptr(0))
-	var data uintptr
+	var data unsafe.Pointer
 	result := Result(C.vkMapMemory(
 		(C.VkDevice)(unsafe.Pointer(device)),
 		(C.VkDeviceMemory)(unsafe.Pointer(memory)),
@@ -380,7 +380,7 @@ func MapMemory(device Device, memory DeviceMemory, offset, size DeviceSize, flag
 	))
 	// fmt.Println("MapMemory(", device, memory, offset, size, flags, ") = ", data, result)
 	if result != Success {
-		return 0, result
+		return nil, result
 	}
 	//return data[idx], nil
 	return data, nil
